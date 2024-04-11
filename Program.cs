96e7f6c8fb5e3522085b9cf498a4751d15b2c61d
Matthew Allison <matthewallisonxyz@gmail.com>
2024-04-11 17:26:29 +0200

    Initial comments written for the main method. User interface class objected created.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
 ﻿namespace MatthewAllison_ST10269378_PRGO6221_POE
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    /// <summary>
+    /// Matthew Allison
+    /// ST10269378
+    /// PROG6221
+    /// POE
+    /// </summary>
+    internal class Program
     {
+        /// <summary>
+        /// This is the main method. The starting point of the program. A UserInterface object is instationated and its
+        /// start method is called to begin the program.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            UserInterface userInterface = new UserInterface();
+            userInterface.Start();
+        }
     }
-  }
+
 }
+//------------------------------------------------------END-OF-FILE---------------------------------------------------//

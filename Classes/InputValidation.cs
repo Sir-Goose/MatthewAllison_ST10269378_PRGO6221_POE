0c60ac29041df002b4a36e8e1db2ecbd533f8fd8
Matthew Allison <matthewallisonxyz@gmail.com>
2024-04-14 16:11:54 +0200

    Implemented custom NumberParsing function to check that a string that is supposed to be used as a number is valid.

--- Classes/InputValidation.cs
+++ Classes/InputValidation.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+
 namespace MatthewAllison_ST10269378_PRGO6221_POE.Classes
 {
     public static class InputValidation
@@ -54,7 +57,7 @@ namespace MatthewAllison_ST10269378_PRGO6221_POE.Classes
             input = input.Trim();
             try
             {
-                float.Parse(input);
+                double.Parse(input);
                 option.Valid = true;
                 option.Value = input;
             }
@@ -63,6 +66,7 @@ namespace MatthewAllison_ST10269378_PRGO6221_POE.Classes
                 option.Valid = false;
                 return option;
             }
+
             return option;
         }
 
@@ -104,6 +108,7 @@ namespace MatthewAllison_ST10269378_PRGO6221_POE.Classes
                 option.Valid = false;
                 return option;
             }
+
             return option;
         }
 
@@ -145,8 +150,10 @@ namespace MatthewAllison_ST10269378_PRGO6221_POE.Classes
                 option.Valid = false;
                 return option;
             }
+
             return option;
         }
+
         /// <summary>
         /// Validates the input by ensuring it is not null and trimming any leading and trailing whitespace.
         /// </summary>
@@ -164,9 +171,98 @@ namespace MatthewAllison_ST10269378_PRGO6221_POE.Classes
             {
                 option.Valid = false;
             }
+
             return option;
         }
+        /// <summary>
+        /// Check that a given string that is meant to be an int is actually valid
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static Option CheckInt(string input)
+        {
+            var (result, number) = ParseNumber<int>(input.Trim());
+            var option = new Option
+            {
+                Valid = result,
+                Value = input
+            };
+
+            return option;
+        }
+        /// <summary>
+        /// A custom built number parser. Parses ints and doubles.
+        /// Checks that any given string is valid as a number.
+        /// Takes in a string and type T which is either int or double.
+        /// Returns two values. First a bool if it was succesful indicating a valid string.
+        /// Second the actual number that was derived. 
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="FormatException"></exception>
+        private static (bool success, T result) ParseNumber<T>(string input)
+        {
+            if (typeof(T) == typeof(int))
+            {
+                try
+                {
+                    var length = input.Length;
+                    var number = 0;
+                    foreach (var character in input)
+                    {
+                        if (char.IsDigit(character))
+                        {
+                            var digit = character - '0';
+                            number += character * 10 ^ length;
+                            --length;
+                        }
+                        else throw new FormatException("Invalid number format");
+                    }
+                    return (true, (T)(object)number);
+                }
+                catch
+                {
+                    return (false, (T)(object)0);
+                }
+            }
+
+            if (typeof(T) != typeof(double)) return (false, (T)(object)0);
+            {
+                try
+                {
+                    var number = 0.0;
+                    if (input.Contains("."))
+                    {
+                        var parts = input.Split('.');
+                        var length = parts[0].Length;
+                        foreach (var character in parts[0])
+                        {
+                            if (Char.IsDigit(character))
+                            {
+                                var digit = character - '0';
+                                number += character * 10 ^ length;
+                                --length;
+                            }
+                            else throw new FormatException("Invalid number format");
+                        }
+                        length = parts[1].Length;
+                        foreach (var character in parts[1].Reverse())
+                        {
+                            var digit = character - '0';
+                            number += character * 10 ^ (-1 * length);
+                            --length;
+                        }
+                    }
+                    return (true, (T)(object)number);
+                }
+                catch
+                {
+                    return (false, (T)(object)0);
+                }
+            }
+        }
     }
-    
 }
 //-------------------------------------------------------END-OF-FILE--------------------------------------------------//
